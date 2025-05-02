1b257999c07d6f17832e34a6fdd48f4508004c54
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2025-05-02 08:18:51 -0300

    🚚 Updated grammar file path

--- include/predictive_parser/module/Grammar.hpp
+++ include/predictive_parser/module/Grammar.hpp
@@ -1,55 +1,57 @@
 #ifndef GRAMMAR_HPP
 #define GRAMMAR_HPP
 
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <set>
 #include "HashUtil.hpp"
-
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 const static std::string ARROW = "::="; // Define o símbolo de produção
 constexpr const char* EPSILON = "ε"; // Define o símbolo de epsilon
-static const std::string GRAMMAR_FILE_PATH = "gramatica_correta.txt"; // Caminho do arquivo de gramática
+static const std::string GRAMMAR_FILE_PATH
+  = "input/gramatica_correta.txt"; // Caminho do arquivo de gramática
 
 struct Production {
-    std::vector<std::string> symbols;
-    bool producesEpsilon;
+   std::vector<std::string> symbols;
+   bool producesEpsilon;
 
-    Production(const std::vector<std::string>& syms, bool epsilon = false)
-        : symbols(syms), producesEpsilon(epsilon) {}
+   Production(const std::vector<std::string>& syms, bool epsilon = false)
+       : symbols(syms), producesEpsilon(epsilon) { }
 };
 
 class Grammar {
-    std::unordered_map<std::string, std::vector<Production>> productionsMap; // Mapa de produções
-    std::set<std::string> nonTerminals;
-    std::set<std::string> terminals;
-    std::string startSymbol; // Símbolo inicial (não-terminal)
+   std::unordered_map<std::string, std::vector<Production>>
+     productionsMap; // Mapa de produções
+   std::set<std::string> nonTerminals;
+   std::set<std::string> terminals;
+   std::string startSymbol; // Símbolo inicial (não-terminal)
 
-    void identifyTerminals();
+   void identifyTerminals();
 
+   public:
+   Grammar();
 
-public:
-    Grammar();
+   void addProduction(const std::string& nonTerminalName,
+     const std::vector<std::string>& production);
 
-    void addProduction(const std::string& nonTerminalName, const std::vector<std::string>& production);
+   const std::unordered_map<std::string, std::vector<Production>>&
+   getProductionsMap() const;
+   void loadFromFile();
 
-    const std::unordered_map<std::string, std::vector<Production>>& getProductionsMap() const;
-    void loadFromFile();
+   const std::set<std::string>& getNonTerminals() const;
 
-    const std::set<std::string>& getNonTerminals() const;
+   const std::set<std::string>& getTerminals() const;
 
-    const std::set<std::string>& getTerminals() const;
+   bool nonTerminalProducesEpsilon(const std::string& nonTerminalName) const;
 
-    bool nonTerminalProducesEpsilon(const std::string& nonTerminalName) const;
+   bool isNonTerminal(const std::string& symbol) const;
 
-    bool isNonTerminal(const std::string& symbol) const;
+   bool isTerminal(const std::string& symbol) const;
 
-    bool isTerminal(const std::string& symbol) const;
+   std::string getStartSymbol() const;
 
-    std::string getStartSymbol() const;
-    
-    void printGrammar() const;
+   void printGrammar() const;
 };
 
 #endif // GRAMMAR_HPP
