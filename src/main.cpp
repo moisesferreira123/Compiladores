dad1a9e31f8d20a2063b01d262436eae5bf6f88c
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2025-05-23 08:42:19 -0300

    🔥 Remove build

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
+#include "parser.hpp"
+#include "scanner.hpp"
+
+#include <fstream>
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from parser!\n";
+int main(int argc, char const *argv[]) {
+  bool debug_flag = (argc >= 2) && (std::strcmp(argv[1], "-d") == 0);
+
+  yy::Scanner scanner{std::cin, std::cout};
+  scanner.set_debug(debug_flag);
+
+  std::ifstream file;
+  if (argc >= 3) {
+    file.open(argv[2]);
+    if (!file.is_open()) {
+      std::cout << "open file {} failed" << argv[2];
+      return 0;
+    }
+    scanner.switch_streams(file, std::cout);
+  }
+
+  yy::Parser parser{scanner};
+  parser.set_debug_level(debug_flag);
+
+  if (parser() == 0) {
+    std::cout << "EXP parse worked";
+  } else {
+    std::cout << "EXP parse failed";
+  }
+  return 0;
 }
